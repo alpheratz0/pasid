3a76a32ccd62dad060c1e72fff6770927aca8a46
alpheratz0 <alpheratz99@protonmail.com>
2022-05-21 19:32:52 -0300

    revert: remove -M option

--- src/pasid.c
+++ src/pasid.c
@@ -17,32 +17,31 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 #include <ctype.h>
 #include <pulse/pulseaudio.h>
 
 #include "debug.h"
 
+#define UNUSED __attribute__((unused))
+
 static char *query = NULL;
 static bool found = false;
-static bool case_sensitive = false;
+static pa_mainloop_api *api;
 
 static bool
-match_substr(const char *src, const char *query, bool cs) {
-	size_t src_len, query_len;
+strcontains(const char *str, const char *x) {
+	int slen, xlen, attempts;
 
-	src_len = strlen(src);
-	query_len = strlen(query);
+	slen = (int)(strlen(str));
+	xlen = (int)(strlen(x));
+	attempts = slen - xlen + 1;
 
-	if (query_len > src_len) {
-		return false;
-	}
-
-	for (size_t n = 0; n < (src_len - query_len + 1); ++n) {
-		for (size_t i = 0; i < query_len; ++i) {
-			if (!cs && tolower(src[n + i]) != tolower(query[i])) break;
-			if (cs && src[n + i] != query[i]) break;
-			if (i == query_len - 1) return true;
+	while (attempts-- > 0) {
+		for (int i = 0; i < xlen; ++i) {
+			if (tolower(str[attempts + i]) != tolower(x[i])) break;
+			if (i == xlen - 1) return true;
 		}
 	}
 
@@ -50,61 +49,59 @@ match_substr(const char *src, const char *query, bool cs) {
 }
 
 static void
-get_sink_input_info_callback(pa_context *c, const pa_sink_input_info *i, int eol, void *userdata) {
-	pa_mainloop_api *api;
-	api = (pa_mainloop_api *)(userdata);
+get_sink_in_cb(pa_context *c, const pa_sink_input_info *i, int eol, UNUSED void *data) {
+	uint32_t 	 sink_id;
+	const char	*sink_appname;
 
 	if (eol < 0) {
-		dief("pa_context_get_sink_input_info_list failed: %s", pa_strerror(pa_context_errno(c)));
-		return;
+		dief("pa_context_get_sink_input_info_list failed: %s",
+				pa_strerror(pa_context_errno(c)));
 	}
 
-	if (eol > 0) {
+	if (eol > 0 || NULL == i) {
 		api->quit(api, 0);
 		return;
 	}
 
-	if (NULL == i) {
-		return;
-	}
+	sink_id = i->index;
+	sink_appname = pa_proplist_gets(i->proplist, "application.name");
 
 	if (NULL == query) {
-		printf("%d - %s\n", i->index, pa_proplist_gets(i->proplist, "application.name"));
+		printf("%d - %s\n", sink_id, sink_appname);
 		return;
 	}
 
-	if (!found) {
-		if (match_substr(pa_proplist_gets(i->proplist, "application.name"), query, case_sensitive)) {
-			printf("%d\n", i->index);
-			found = true;
-		}
+	if (!found && (found = strcontains(sink_appname, query))) {
+		printf("%d\n", sink_id);
 	}
 }
 
 static void
-context_ready_callback(pa_context *c, void *userdata) {
-	pa_operation_unref(pa_context_get_sink_input_info_list(c, get_sink_input_info_callback, userdata));
+context_ready_cb(pa_context *c) {
+	pa_operation_unref(
+		pa_context_get_sink_input_info_list(
+			c, get_sink_in_cb, NULL
+		)
+	);
 }
 
 static void
-context_state_callback(pa_context *c, void *userdata) {
-	pa_mainloop_api *api;
-	api = (pa_mainloop_api *)(userdata);
-
+context_state_cb(pa_context *c, UNUSED void *data) {
 	switch (pa_context_get_state(c)) {
 		case PA_CONTEXT_CONNECTING:
 		case PA_CONTEXT_AUTHORIZING:
 		case PA_CONTEXT_SETTING_NAME:
 			break;
 		case PA_CONTEXT_READY:
-			context_ready_callback(c, userdata);
+			context_ready_cb(c);
 			break;
 		case PA_CONTEXT_TERMINATED:
 			api->quit(api, 0);
 			break;
 		case PA_CONTEXT_FAILED:
 		default:
-			dief("pa_context_connect failed: %s", pa_strerror(pa_context_errno(c)));
+			dief("pa_context_connect failed: %s",
+					pa_strerror(pa_context_errno(c)));
 			break;
 	}
 }
@@ -117,10 +114,9 @@ match_opt(const char *in, const char *sh, const char *lo) {
 
 static void
 usage(void) {
-	puts("Usage: pasid [ -hv ] [ -mM QUERY ]");
+	puts("Usage: pasid [ -hv ] [ -m QUERY ]");
 	puts("Options are:");
 	puts("     -m | --match                   get the sink id of the application that matches the query");
-	puts("     -M | --match-case-sensitive    the same as above but case sensitive");
 	puts("     -h | --help                    display this message and exit");
 	puts("     -v | --version                 display the program version");
 	exit(0);
@@ -141,12 +137,10 @@ main(int argc, char **argv) {
 		if (match_opt(*argv, "-h", "--help")) usage();
 		else if (match_opt(*argv, "-v", "--version")) version();
 		else if (match_opt(*argv, "-m", "--match") && --argc > 0) query = *++argv;
-		else if (match_opt(*argv, "-M", "--match-case-sensitive") && --argc > 0) { query = *++argv; case_sensitive = true; }
 		else if (**argv == '-') dief("invalid option %s", *argv);
 		else dief("unexpected argument: %s", *argv);
 	}
 
-	pa_mainloop_api *api;
 	pa_mainloop *m;
 	pa_context *context;
 
@@ -154,7 +148,7 @@ main(int argc, char **argv) {
 	api = pa_mainloop_get_api(m);
 	context = pa_context_new(api, NULL);
 
-	pa_context_set_state_callback(context, context_state_callback, (void *)(api));
+	pa_context_set_state_callback(context, context_state_cb, NULL);
 	pa_context_connect(context, NULL, 0, NULL);
 
 	/* retval not used */
